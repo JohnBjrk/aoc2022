003c9f581772cffdcb7d8d335e0736a76b490595
John Björk <john.m.bjork@gmail.com>
2022-12-02 06:07:34 +0100

    ♻️ Day 01 with Streams

--- elixir_aoc/lib/day01.ex
+++ elixir_aoc/lib/day01.ex
@@ -13,16 +13,43 @@ defmodule Day01 do
       end)
       |> Enum.sum()
     end)
+  end
 
+  defp calories_per_elf2(calorie_table) do
+    calorie_table
+    |> parse()
+    |> chunk_by_elf()
+    |> Enum.to_list()
+    |> Enum.map(&Enum.sum/1)
+  end
+
+  defp parse(lines) do
+    lines
+    |> Stream.map(fn line ->
+      case line do
+        "" ->
+          :next
+
+        calorie_string ->
+          {calorie, _} = Integer.parse(calorie_string)
+          calorie
+      end
+    end)
+  end
+
+  defp chunk_by_elf(calorie_list) do
+    calorie_list
+    |> Stream.chunk_by(&(&1 == :next))
+    |> Stream.filter(&(&1 != [:next]))
   end
 
   def part1(calorie_table) do
-    calories_per_elf(calorie_table)
+    calories_per_elf2(calorie_table)
     |> Enum.max()
   end
 
   def part2(calorie_table) do
-    calories_per_elf(calorie_table)
+    calories_per_elf2(calorie_table)
     |> Enum.sort(&(&1 >= &2))
     |> Enum.take(3)
     |> Enum.sum()
--- elixir_aoc/lib/helpers.ex
+++ elixir_aoc/lib/helpers.ex
@@ -1,6 +1,6 @@
 defmodule Helpers do
   def read_lines!(file_path) do
     File.stream!(file_path, [:utf8], :line)
-      |> Enum.map(&(String.trim(&1)))
+      |> Stream.map(&(String.trim(&1)))
   end
 end
